d8b5512a6b9bb7e0a5f90643e17e0ddc64b3785e
ateist123 <83414705+ateist123@users.noreply.github.com>
2022-11-10 22:45:32 +0300

    v1.0.4

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import os
-#import evil_module
 class shell():
-    def __init__(self,cmd_in=[]):
-        self.cmd = cmd_in
-    def mkfile(fname):
-        with open(fname,'w') as f:
-            pass
-    def runshell(self,cmd=[]):
-        if cmd == []:cmd=self.cmd
-        filename = 'return.txt' #const str
-        self.mkfile(filename)
+    def mkfile(fname):                    #creating file
+        with open(fname,'w') as f:pass    #magic
+    def runshell(self,cmd=[]):            
+        if cmd == []:cmd=self.cmd         #if no args inputed, using class init args
+        filename = 'return.txt'           #const str
+        self.mkfile(filename)             #creating out file
         c = 0
-        for x in cmd:
-            if c==0:
-                os.system(x +' >> '+filename)
+        for x in cmd:                         #parsing args
+            if c==0:                          #if first iteration changing out
+                os.system(x +' >> '+filename) #changing output
             else:
-                os.system(x +' > '+filename)
+                os.system(x +' > '+filename)  #appending output
             c+=1
-        with open(filename,'r') as f:print(f.read())
+        with open(filename,'r') as f:print(f.read()) #reading output
 
 shell().mkfile('p.txt')
 shell().runshell(['cat main.py>>p.txt'])
